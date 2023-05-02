3d8cdca14de6ab8fbb8da39bdb40e8e9cd3f3ebc
Joshua Warren <josh@creatuity.com>
2023-05-02 08:30:20 -0500

    Reworking the method used to fetch podcast feeds to be compatible with running inside Obsidian.

--- cli.ts
+++ cli.ts
@@ -10,10 +10,11 @@ const personalInfo = fs.readFileSync(personalInfoPath, 'utf-8').trim();
 // Replace these values with your actual data or read them from a config file or environment variables
 const podcastFeedUrl = 'https://feeds.feedburner.com/ChaseOaksChurch';
 const ffmpegPath = '/opt/homebrew/bin/ffmpeg'; // Optional
+const storeFiles = true; // Store files to the filesystem or not?
 
 (async () => {
     try {
-        const takeaway = await fetchAndSummarize(apiKey, personalInfo, podcastFeedUrl, ffmpegPath);
+        const takeaway = await fetchAndSummarize(apiKey, personalInfo, podcastFeedUrl, storeFiles, ffmpegPath);
         console.log('Takeaway:', takeaway);
     } catch (error) {
         console.error('Error:', error);
